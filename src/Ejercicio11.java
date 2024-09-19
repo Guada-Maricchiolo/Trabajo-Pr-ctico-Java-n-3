8ed35c89843aca398270e872fca4a15e2ee80150
barrera-miguel <miguelbo138@gmail.com>
2024-09-19 15:41:39 -0300

    Cambios en ejercicio11

--- src/Ejercicio11.java
+++ src/Ejercicio11.java
@@ -8,22 +8,33 @@ public class Ejercicio11 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ingrese lo valores del primer arreglo");
+        System.out.println("Ingrese los valores del primer arreglo:");
         for (int i = 0; i < numeros.length; i++){
             numeros[i] = sc.nextInt();
         }
 
-        System.out.println("Ingrese lo valores del segundo arreglo");
+        System.out.println("Ingrese los valores del segundo arreglo:");
         for (int i = 0; i < numeros2.length; i++){
             numeros2[i] = sc.nextInt();
         }
 
+        System.out.println("Arreglo 1:");
+        for(int n : numeros) {
+            System.out.print(n + " ");
+        }
+
+        System.out.println("\nArreglo 2:");
+        for(int n : numeros2) {
+            System.out.print(n + " ");
+        }
+
+        System.out.println("\nArreglo 3:");
         int[] resultado = new int[5];
         for (int i = 0; i < numeros.length; i++){
             for (int j = 0; j < numeros2.length; j++){
                 resultado[i] += numeros[i] * numeros2[j];
             }
-            System.out.println(resultado[i]);
+            System.out.print(resultado[i] + " ");
         }
     }
 }
